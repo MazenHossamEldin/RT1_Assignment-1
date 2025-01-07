99747d6e32f0a3a68180b6646739694cf5ed3de3
mazen madbouly <mazen.madbouly01@gmail.com>
2025-01-07 01:36:28 +0000

    Modify to stop the moving turtle when it is near to the boundary or the other turtle

--- src/assignment1_rt/scripts/distance.py
+++ src/assignment1_rt/scripts/distance.py
@@ -3,60 +3,59 @@
 import rospy
 from turtlesim.msg import Pose
 from std_msgs.msg import Float32
-from rospy.core import rospy  # Import rospy.loginfo
+from geometry_msgs.msg import Twist
+from rospy.core import rospy
 
 class DistanceMonitor:
-    """
-    This class defines a DistanceMonitor object that monitors the distance between two turtles in a ROS simulation.
-    """
-
     def __init__(self):
-        """
-        This is the constructor of the DistanceMonitor class. It is called when an instance of the class is created.
-        """
         rospy.init_node('distance_node', anonymous=True)
 
         self.turtle1_pose = Pose()
         self.turtle2_pose = Pose()
-        self.threshold = 1.0
+        self.threshold_distance = 1.0  # Distance threshold for proximity warning
+        self.boundary_threshold = 1.0  # Distance threshold for boundary proximity
 
         rospy.Subscriber('/turtle1/pose', Pose, self.turtle1_callback)
         rospy.Subscriber('/turtle2/pose', Pose, self.turtle2_callback)
-
-        self.pub = rospy.Publisher('/turtle_distance', Float32, queue_size=10)
+        self.distance_pub = rospy.Publisher('/turtle_distance', Float32, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)  # Publish to turtle1's velocity
 
     def turtle1_callback(self, data):
-        """
-        This function is called whenever a new message is received on the '/turtle1/pose' topic.
-        """
         self.turtle1_pose = data
 
     def turtle2_callback(self, data):
-        """
-        This function is called whenever a new message is received on the '/turtle2/pose' topic.
-        """
         self.turtle2_pose = data
 
     def compute_distance(self):
-        """
-        This function calculates the Euclidean distance between the poses of turtle1 and turtle2.
-        """
         dx = self.turtle1_pose.x - self.turtle2_pose.x
         dy = self.turtle1_pose.y - self.turtle2_pose.y
         return (dx**2 + dy**2)**0.5
 
-    def monitor(self):
-        """
-        This function continuously monitors the distance between the turtles and publishes the distance and warning messages if necessary.
-        """
-        rate = rospy.Rate(10)
+    def check_boundary_proximity(self):
+        if (self.turtle1_pose.x < self.boundary_threshold or 
+            self.turtle1_pose.x > 11 - self.boundary_threshold or 
+            self.turtle1_pose.y < self.boundary_threshold or 
+            self.turtle1_pose.y > 11 - self.boundary_threshold):
+            return True
+        return False
 
+    def monitor(self):
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             distance = self.compute_distance()
-            self.pub.publish(distance)
+            self.distance_pub.publish(distance)
 
-            if distance < self.threshold:
-                rospy.loginfo("Distance between turtles: %s", distance)  # Fixed indentation and closing parenthesis
+            if distance < self.threshold_distance:
+                rospy.loginfo("Turtles are too close!")
+                # Stop turtle1 if it's too close to turtle2
+                cmd_vel_msg = Twist()
+                self.cmd_vel_pub.publish(cmd_vel_msg) 
+
+            if self.check_boundary_proximity():
+                rospy.loginfo("Turtle1 is too close to the boundary!")
+                # Stop turtle1 if it's too close to the boundary
+                cmd_vel_msg = Twist()
+                self.cmd_vel_pub.publish(cmd_vel_msg) 
 
             rate.sleep()
 
